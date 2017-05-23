5fbe5057c7cd2713f0c9e01f3b3a6adf88c48a24
ramsberg <olle@ramsberg.nu>
2017-05-23 20:27:33 +0200

    Added func libb

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+import olles_libb
 
 print("Hello World!")
+
+
+result = olles_libb.addera( 5, 10 )
+
+print(result)
 
---
+++ olles_libb.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+
+def addera(tal1, tal2):
+	return tal1 + tal2
+
